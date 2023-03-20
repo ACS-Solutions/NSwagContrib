ed68b1c1110b61e777e2074c1a5c70520ef54330
Alasdair Cunningham-Smith <alasdair@acs-solutions.co.uk>
2023-03-20 18:28:36 +0000

    Treat return of ValueTask<T> as T, same as Task<T> etc Unify return type unwrapping

--- src/NSwag.Generation/OpenApiSchemaGenerator.cs
+++ src/NSwag.Generation/OpenApiSchemaGenerator.cs
@@ -7,6 +7,7 @@
 //-----------------------------------------------------------------------
 
 using System;
+
 using Namotion.Reflection;
 using NJsonSchema;
 using NJsonSchema.Generation;
@@ -62,18 +63,7 @@ namespace NSwag.Generation
             ContextualType contextualType, bool isNullable,
             JsonSchemaResolver schemaResolver, Action<TSchemaType, JsonSchema> transformation = null)
         {
-            if (contextualType.TypeName == "Task`1")
-            {
-                contextualType = contextualType.OriginalGenericArguments[0];
-            }
-            else if (contextualType.TypeName == "JsonResult`1")
-            {
-                contextualType = contextualType.OriginalGenericArguments[0];
-            }
-            else if (contextualType.TypeName == "ActionResult`1")
-            {
-                contextualType = contextualType.OriginalGenericArguments[0];
-            }
+            GenericResultWrapperTypes.RemoveGenericWrapperTypes (ref contextualType,t=>t.TypeName,t=>t.OriginalGenericArguments[0]);
 
             if (IsFileResponse(contextualType))
             {
